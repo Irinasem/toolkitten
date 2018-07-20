b3c8db1671f28381ec4f8613a13c1d6ab7300af7
Irinasem <41349047+Irinasem@users.noreply.github.com>
2018-07-19 20:40:14 -0700

    Create soc-wk1-cert-Irina-Semichina.py

---
+++ summer-of-code/week-01/wk1-homework-submission/soc-wk1-cert-Irina-Semichina.py
@@ -0,0 +1,23 @@
+first_name='Irina'
+print('What are you first name?' + first_name +'.')
+What are you first name? Irina
+last_name= 'Semichina'
+print('What are you last name?' + last_name + '.')
+What are you last name? Semichina.
+print(first_name + last_name)
+Irina Semichina
+favorite_number = '7'
+bigger_number = int(favorite_number) + 1
+print (bigger_number)
+8
+print(favorite_number)
+7
+employee_wish = 'I WANT A RAISE !'
+boss_answer = 'YOU\RE FIRED !'
+print(employee_wish + boss_answer)
+I WANT A RAISE ! YOU\RE FIRED !
+Chapter_1 = ' Getting Started page 1 '
+Chapter_2 = ' Numbers page 9 '
+Chapter_3 = ' Letters page 13 '
+print ( ' Chapter 1 : ' + Chapter_1 + ' Chapter 2 : ' + Chapter_2 + ' Chapter 3 : ' + Chapter_3 + '.' )
+Chapter 1: Getting Started page1 Chapter 2: Numbers page9 Chapter 3: Letters page 13.
